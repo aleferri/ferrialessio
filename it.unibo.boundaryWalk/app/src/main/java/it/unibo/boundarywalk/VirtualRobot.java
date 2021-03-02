3ed6cad55efe1e26e5afce52f6fa0ae206554b38
Alessio Ferri <alessio.ferri.3012@gmail.com>
2021-03-02 10:47:20 +0100

    Update VirtualRobot.java

--- it.unibo.boundaryWalk/app/src/main/java/it/unibo/boundarywalk/VirtualRobot.java
+++ it.unibo.boundaryWalk/app/src/main/java/it/unibo/boundarywalk/VirtualRobot.java
@@ -1,1 +1,56 @@
+package it.unibo.boundaryWalk;
 
+import java.util.function.Consumer;
+
+public interface VirtualRobot {
+    
+    public boolean moveForward(int duration);
+    
+    public boolean moveLeft(int duration);
+    
+    public default void boundaryWalk(Consumer<Integer> observer) {
+        int d = 300;
+
+        boolean collided;
+        do {
+            collided = this.moveForward(d);
+            if ( ! collided ) {
+                observer.accept(d);
+            }
+        } while(!collided);
+
+        this.moveLeft( 300 );
+        observer.accept(-1);
+
+        do {
+            collided = this.moveForward(d);
+            if ( ! collided ) {
+                observer.accept(d);
+            }
+        } while(!collided);
+
+        this.moveLeft(300);
+        observer.accept(-1);
+
+        do {
+            collided = this.moveForward(d);
+            if ( ! collided ) {
+                observer.accept(d);
+            }
+        } while(!collided);
+
+        this.moveLeft(300);
+        observer.accept(-1);
+
+        do {
+            collided = this.moveForward(d);
+            if ( ! collided ) {
+                observer.accept(d);
+            }
+        } while(!collided);
+
+        this.moveLeft(300);
+        observer.accept(-1);
+    }
+  
+}
